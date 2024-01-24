e2a78d31528979844a3d50adb276326987b92659
Danillo Soares <danillohss@gmail.com>
2024-01-24 18:17:22 -0300

    Adicionando componentes Cards/botão/inputs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -30,25 +31,90 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Despesas Pessoais."),
+        title: const Text("Despesas Pessoais."),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            child: Card(
-              child: Text('Gráfico'),
+            child: const Card(
               elevation: 5,
+              child: Text('Gráfico'),
             ),
           ),
           Column(
             children: _transactions.map((tr) {
               return Card(
-                child: Text(tr.title),
-              );
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${tr.value.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tr.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat(' d / MMM / y ').format(tr.date),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ));
             }).toList(),
-          )
+          ),
+          const Card(
+              elevation: 5,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Valor (R\$)',
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(Colors.purple),
+                      ),
+                      onPressed: null,
+                      child: Text(
+                        'Cadastrar',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ))
         ],
       ),
     );
